2c9915d93207664b35e2d4c8144283bd827f9958
mochk1 <maor.wonderz@gmail.com>
2021-06-26 16:35:44 +0300

    env

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const fetch = require('node-fetch');
 const cors  = require('cors')
-const port =  5000
+require('dotenv').config();
+const port = process.env.PORT || 5000
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
