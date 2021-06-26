246dc622daeae68438548e0f70fda9a82532e3ba
Maor Killner <40217999+mochk1@users.noreply.github.com>
2021-06-26 20:13:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const port = process.env.PORT || 5000
 const cors  = require('cors')
 const fetch = require('node-fetch');
-
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
