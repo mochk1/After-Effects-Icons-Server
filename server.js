7925701d152b06bfb7b2cbcdebc18139652753a2
mochk1 <maor.wonderz@gmail.com>
2021-06-26 16:18:16 +0300

    server-upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const fetch = require('node-fetch');
+const cors  = require('cors')
+const port =  5000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors());
+
+var NounProject = require('the-noun-project'),
+nounProject = new NounProject({
+    key: '778a4df57947483c8969ab9efa302e7a',
+    secret: '3a9a4ee255b84bee812d31516255b352'
+});
+
+
+app.post('/icons', async (req, res) => {
+console.log(req.body.term)
+   nounProject.getIconsByTerm(req.body.term, {limit: 50}, function (err, data) {
+    if (!err) {
+        res.json(data.icons);
+    }
+  }); 
+
+ })
+
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}`))
